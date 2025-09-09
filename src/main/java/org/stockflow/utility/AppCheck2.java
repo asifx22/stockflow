0540c17ed106bbddb5f5796bd95c11aeeff3a255
Asif <asif2299ansari@gmail.com>
2025-09-10 00:42:28 +0530

    masterchanges

---
+++ src/main/java/org/stockflow/utility/AppCheck2.java
@@ -0,0 +1,25 @@
+package org.stockflow.utility;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("stockflow")
+
+public class AppCheck2 {
+
+    public static final Logger logger = LoggerFactory.getLogger(AppCheck.class);
+
+
+    @GetMapping
+    public String getStatus() {
+        logger.error("Application is running");
+        System.out.println("Live");
+        return "Live";
+    }
+}
